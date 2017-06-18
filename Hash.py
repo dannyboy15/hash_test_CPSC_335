1b718efbd584e071787a9c926d3a37108eb0f64e
Daniel Bravo <bravoappdev@gmail.com>
2017-06-18 01:41:29 -0700

    Initial commit of Python files. Working copy

---
+++ Hash.py
@@ -0,0 +1,21 @@
+'''
+    An implementation of a hash function
+'''
+
+
+def hash(inStr, radix, modulus):
+    '''
+        Complete the implementation of the method hash(); be sure to
+        uncomment the value returned!
+
+        Use (ord)inStr[i] to convert the character at position i
+        to the integer value of its ASCII code.
+
+        NOTE that the indexing of the characters in a Python string is reversed
+        from what we used in lecture.
+    '''
+    hashValue = 0
+    for i in range(1, len(inStr) - 1):
+        hashValue = (hashValue * radix + ord(inStr[i])) % modulus
+
+    return hashValue
