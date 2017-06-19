7f4d62eb1842eccc8680c411f45cbf5e43f5f351
Daniel Bravo <bravoappdev@gmail.com>
2017-06-18 21:01:20 -0700

    Fixed TestHash. Has same code as Hash.

--- TestHash.py
+++ TestHash.py
@@ -1,21 +1,30 @@
 '''
-    An implementation of a hash function
+   A program to test hash function variations
 '''
+# import Hash.py
+from Hash import *
 
 
-def hash(inStr, radix, modulus):
-    '''
-        Complete the implementation of the method hash(); be sure to
-        uncomment the value returned!
+def testHash(radix, modulus, fName):
+    print()
+    print("Using radix " + str(radix) + " and modulus " + str(modulus) + ".")
+    print()
+    print("  Input  |  hash value")
+    print("----------------------")
+    file = open(fName)
+    for line in file:
+        for word in line.strip().split(' '):
+            if (word != ''):
+                print('{0:10s} {1:8d}'.format(word, hash(word, radix, modulus)))
+    print()
 
-        Use (ord)inStr[i] to convert the character at position i
-        to the integer value of its ASCII code.
 
-        NOTE that the indexing of the characters in a Python string is reversed
-        from what we used in lecture.
-    '''
-    hashValue = 0
-    for i in range(1, len(inStr) - 1):
-        hashValue = (hashValue * radix + ord(inStr[i])) % modulus
+def run_test():
+    while True:
+        inp = input("Enter the radix, modulus, and file: ")
+        r, m, f = inp.split(',')
+        testHash(int(r), int(m), f.lstrip())
 
-    return hashValue
+
+if __name__ == "__main__":
+    run_test()
